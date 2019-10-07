5b08aac142f74abbf6e7d52c9c03afbb3e1473cb
David Walker <dwalker0044@gmail.com>
2019-10-07 19:06:23 +0100

    Removed 2d discrete as this is not used.

--- lunarlander/main.py
+++ lunarlander/main.py
@@ -1,24 +1,33 @@
-import gym
-import math
 import numpy as np
 from plotmap import plot_map
 import matplotlib.pyplot as plt
+from typing import Callable
 
-EPSILON = 0.3
+# EPSILON is the exploration rate, the higher this is the more the agent will explore.
+EPSILON = 0.6
+# ALPHA and GAMMA relate to the learning update function.
 ALPHA = 0.1
 GAMMA = 0.6
-NUM_EPISODES = 20000
 
+# Total number of episodes to train for.
+NUM_EPISODES = 2000
+# Displays animation once per one hundred episodes.
 RENDER_ANIMATION = False
+
 from lunarlandercustom import LunarLanderCustom
-from qtablediscrete import QTable2dDiscrete
+from qtablediscrete import QTable1dDiscrete
 
 
-def run(env, q_table, state, epsilon=EPSILON):
+def run(env: LunarLanderCustom,
+        q_table: QTable1dDiscrete,
+        state: np.array,
+        epsilon: float = EPSILON):
     if np.random.uniform(0, 1) < epsilon:
-        action = env.action_space.sample()  # Explore action space
+        # Explore action space
+        action = env.action_space.sample()
     else:
-        action = np.argmax(q_table.lookup_state(state))  # Exploit learned values
+        # Exploit learned values
+        action = np.argmax(q_table.lookup_state(state))
 
     next_state, reward, done, _ = env.step(action)
     reward = reward[1]
@@ -27,7 +36,12 @@ def run(env, q_table, state, epsilon=EPSILON):
     return done, reward, next_state
 
 
-def run_episodes(env, q_table, state_0, n_episodes=NUM_EPISODES, epsilon=EPSILON, render_pred=None):
+def run_episodes(env: LunarLanderCustom,
+                 q_table: QTable1dDiscrete,
+                 state_0: np.array,
+                 render_pred: Callable[[int], bool],
+                 n_episodes: int = NUM_EPISODES,
+                 epsilon: float = EPSILON, ):
     rewards = np.zeros(n_episodes)
     episode_reward = 0
     episode_count = 0
@@ -73,37 +87,38 @@ def moving_average(a, n=10):
 
 
 def main():
-    # env = gym.make("LunarLander-v2")
     env = LunarLanderCustom()
 
-    # The visible area of the map has been normalize to be between +/-1. We go up to +/-2 as the
-    # lander can go off the screen.
-    y_bins = np.arange(-5, 20, 0.5)
-    x_bins = np.arange(-10, 10, 0.5)
-    q_table = QTable2dDiscrete(3, 1, x_bins, y_bins, env.action_space.n)
+    # Create a q-table that captures one dimension (angle) of the state space.
+    # Since angle is continuous it needs to be discretize as we cannot store an infinite number of states.
+    x_bins = np.arange(-2 * np.pi, 2 * np.pi, (4 * np.pi) / 720)
+    q_table = QTable1dDiscrete(4, x_bins, env.action_space.n)
 
     state = env.reset()
 
+    # Now run the training episodes.
     rewards = run_episodes(env,
                            q_table,
                            state,
                            render_pred=lambda x: RENDER_ANIMATION and x % 100 == 0)
-    print("------------------------------------")
+
+    print("-" * 20)
+
+    # Run 5 episodes using only the solution, i.e 0 learning rate, so we can see the performance of our agent.
     run_episodes(env,
                  q_table,
                  state,
+                 render_pred=lambda x: True,
                  n_episodes=5,
-                 epsilon=0.0,
-                 render_pred=lambda x: True)
+                 epsilon=0.0)
 
     env.close()
 
-    shape = (y_bins.shape[0], x_bins.shape[0])
-
-    plot_map(q_table.q_table, shape)
+    plot_map(q_table.q_table)
 
     plt.figure(2)
     plt.plot(moving_average(rewards))
+    plt.title("Reward per episode")
     plt.draw()
     plt.show()
 
--- lunarlander/qtablediscrete.py
+++ lunarlander/qtablediscrete.py
@@ -5,24 +5,21 @@ ALPHA = 0.1
 GAMMA = 0.6
 
 
-class QTable2dDiscrete:
-    def __init__(self, x_state, y_state, x_bins, y_bins, n_action_space):
+class QTable1dDiscrete:
+    def __init__(self, x_state, x_bins, n_action_space):
         self.x_state = x_state
-        self.y_state = y_state
         self.x_bins = x_bins
-        self.y_bins = y_bins
 
-        shape = (self.x_bins.shape[0] * self.y_bins.shape[0], n_action_space)
+        shape = (self.x_bins.shape[0], n_action_space)
         self.q_table = np.zeros(shape)
 
     def lookup_state(self, state):
         # Discretize the continuous position of the lander.
-        x = np.digitize(state[self.x_state] * 10, self.x_bins)
-        y = np.digitize(state[self.y_state] * 10, self.y_bins)
+        state_value = state[self.x_state]
+        state_index = np.digitize(state_value, self.x_bins)
 
-        state_index = y * self.x_bins.shape[0] + x
-
-        assert 0 <= state_index <= self.x_bins.shape[0] * self.y_bins.shape[0], "OoB array index."
+        assert 0 <= state_index < self.x_bins.shape[0], \
+            f"OoB array index {state_index}. State value was {state_value}"
         return self.q_table[state_index]
 
     def update(self, state, next_state, action, reward):
